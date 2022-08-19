22f86316617b20e2277005be450c754bcba223d6
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2022-08-19 17:58:24 +0530

    Update class_in_python.py
    adding a class function to determine if a mobile is expensive

--- class_in_python.py
+++ class_in_python.py
@@ -4,9 +4,17 @@ class mobile_phone :
     self.price = price
     self.color = color
     self.is_available = is_available
+    
+  #creating a class function to determine if a mobile is expensive
+  def expensive(self) :
+    if self.price > 20000:
+      return True
+    else:
+      return False  
 
 my_mobile = mobile_phone("nokia",10500,"red",True)
 print(my_mobile.brand)
 print(my_mobile.color)
 print(my_mobile.price)
+print(my_mobile.expensive())
     
